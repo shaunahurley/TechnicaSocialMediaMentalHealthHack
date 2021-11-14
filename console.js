3dc1efdb1a70857eea99599c8b3e12f6b593b2c8
shaunahurley <72588201+shaunahurley@users.noreply.github.com>
2021-11-14 00:50:14 -0500

    Update console.js

--- console.js
+++ console.js
@@ -1,1 +1,1 @@
-document.getElementById("_8P13R").innerHTML = "testing";
+chrome.runtime.onInstalled.addListener(() => { chrome.storage.sync.set({document.getElementByClassName("_8P13R").innerHTML});});
